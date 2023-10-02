0345dfdacff518acc2bf8f47c830f0f1e2178132
Feras532 <saf_523_@hotmail.com>
2023-10-02 22:40:06 +0300

    CSS 04 - Ex 1,3,4
    complete positioning
    mobile styling
    and desktop styling
    according to my chosen figma style

--- myWeb/script.js
+++ myWeb/script.js
@@ -1,6 +1,12 @@
-window.onload = function() {
-    setTimeout(function() {
-        document.getElementById('loader').style.display = "none";
-        document.getElementById('content').style.display = "block";
-    }, 2000);
-}
+// window.onload = function() {
+//     setTimeout(function() {
+//         document.getElementById('loader').style.display = "none";
+//         document.getElementById('content').style.display = "block";
+//     }, 2000);
+// }
+const hamburger = document.querySelector('.hamburger');
+const navItems = document.querySelector('.nav-items');
+
+hamburger.addEventListener('click', () => {
+  navItems.style.display = navItems.style.display === 'none' ? 'block' : 'none';
+});
