81d46739861144b0afc655a640a2a13fd365a514
Feras532 <saf_523_@hotmail.com>
2023-10-08 20:07:32 +0300

    js 2 -> exercise 2

--- myWeb/script.js
+++ myWeb/script.js
@@ -51,4 +51,29 @@ window.onload = function () {
 
     body.appendChild(card); // Add the card to the body
   });
+
+  // java script 02 -> exercise 2
+
+  alert(
+    "Press keys 0-3 to navigate through the pages:\n0: Arabic\n1: Samples\n2: Order\n3: About"
+  );
+
+  document.body.addEventListener("keydown", function (event) {
+    // This line returns a list of links.
+    const navLinks = document.querySelectorAll(".nav-items a");
+
+    // Check if the key pressed is a number between '0' and '3'
+    if (event.key >= "0" && event.key <= "3") {
+      const linkIndex = parseInt(event.key);
+
+      // Get the link at the given index. This is the link we want to navigate to.
+      const link = navLinks[linkIndex];
+
+      // Get the URL
+      const url = link.href;
+
+      // Navigate to the URL.
+      window.location.href = url;
+    }
+  });
 };
