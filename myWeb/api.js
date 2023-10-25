5af767cedb5748f03b623f945bf9bbcb332479c6
Feras532 <saf_523_@hotmail.com>
2023-10-25 23:26:52 +0300

    js04 --> exercise 2

--- myWeb/api.js
+++ myWeb/api.js
@@ -1,29 +1,36 @@
-// api.js
-async function fetchArtworks() {
+// Using function expression
+const fetchArtworks = async function () {
   const api_url = "https://api.artic.edu/api/v1/artworks?page=1&limit=10";
   const response = await fetch(api_url);
   const data = await response.json();
   return data.data;
-}
+};
 
-window.onload = async function () {
+// Using nested functions
+const createAndAppendImage = function (artwork, container) {
+  const img = document.createElement("img");
+  img.src = `https://www.artic.edu/iiif/2/${artwork.image_id}/full/843,1000/0/default.jpg`;
+  // img.alt = artwork.title;
+  img.style.height = "400px";
+  container.append(img);
+};
+
+// Using IIFE and arrow syntax
+
+window.onload = (async () => {
   const artworks = await fetchArtworks();
   const artworkContainer = document.querySelector("#artwork-container");
 
   let count = 0;
   for (const artwork of artworks) {
     if (artwork.image_id) {
-      const img = document.createElement("img");
-      img.src = `https://www.artic.edu/iiif/2/${artwork.image_id}/full/843,1000/0/default.jpg`;
-      img.alt = artwork.title;
-      img.style.height = "400px";
-      artworkContainer.append(img);
+      createAndAppendImage(artwork, artworkContainer);
       count++;
-      if (count >= 3) {
+      if (count >= 10) {
         break;
       }
     } else {
       console.log(`Artwork with ID ${artwork.id} has no image.`);
     }
   }
-};
+})();
