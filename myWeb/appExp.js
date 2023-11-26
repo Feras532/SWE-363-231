4126d3fabb6551bdb17272d1fd0387431a8c8f6c
Feras532 <saf_523_@hotmail.com>
2023-11-26 23:58:03 +0300

    BE03 --> Ex04

--- myWeb/appExp.js
+++ myWeb/appExp.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = 1000;
+const port = 5500;
 
 app.use(express.static(__dirname));
 
+// This middleware will parse the form data
+app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "home.html"));
 });
@@ -20,6 +22,19 @@ app.get("/aboutMe", (req, res) => {
   res.sendFile(path.join(__dirname, "aboutMe.html"));
 });
 
+// Middleware to parse POST request bodies
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+// POST endpoint at /submitOrder
+app.post("/submitOrder", (req, res) => {
+  console.log(req.body); // Logs the request body to the console
+  res.status(200).send("Form submission received!");
+});
+
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server listening at http://localhost:${port}`);
 });
